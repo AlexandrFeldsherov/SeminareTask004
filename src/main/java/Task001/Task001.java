124e18f2810602d322545a908a6ffcf79736981c
[AlexandrFeldsherov] <[alexandrfeldsherov@mail.ru]>
2023-05-02 10:41:27 +0300

    init commit

---
+++ src/main/java/Task001/Task001.java
@@ -0,0 +1,14 @@
+package Task001;
+
+public class Task001 {
+    /**
+     * 1)Вывести список на экран в перевернутом виде (без массивов и ArrayList)
+     * Пример:
+     * 1 -> 2->3->4
+     * Вывод:
+     * 4->3->2->1
+     */
+    public static void main(String[] args) {
+
+    }
+}
