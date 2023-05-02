ad89f3a2acdf205baeb477ea1b06e720bc82e5b5
[AlexandrFeldsherov] <[alexandrfeldsherov@mail.ru]>
2023-05-02 19:23:22 +0300

    array result task002

--- src/main/java/Task001/Task001.java
+++ src/main/java/Task001/Task001.java
@@ -1,5 +1,9 @@
 package Task001;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+
 public class Task001 {
     /**
      * 1)Вывести список на экран в перевернутом виде (без массивов и ArrayList)
@@ -9,6 +13,8 @@ public class Task001 {
      * 4->3->2->1
      */
     public static void main(String[] args) {
-
+        LinkedList<Integer> lst = new LinkedList<>(Arrays.asList(1, 2, 3, 4));
+        Collections.reverse(lst);
+        System.out.println("lst = " + lst);
     }
 }
